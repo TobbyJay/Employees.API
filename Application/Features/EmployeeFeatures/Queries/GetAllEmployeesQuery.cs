5443aa16211c5638ad8650b2ab405b2735cd5199
TobbyJay <umohjunior96@gmail.com>
2022-01-05 13:54:46 +0100

    added query to get all employees

---
+++ Application/Features/EmployeeFeatures/Queries/GetAllEmployeesQuery.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using MediatR;
+
+using Entities;
+using Entities.Context;
+
+namespace Application.Features.EmployeeFeatures.Queries
+{
+    public class GetAllEmployeesQuery : IRequest<IEnumerable<Employee>>
+    {
+        public class GetAllEmployeesHandler : IRequestHandler<GetAllEmployeesQuery, IEnumerable<Employee>>
+        {
+            private readonly AppDbContext _context;
+            public GetAllEmployeesHandler(AppDbContext context)
+            {
+                _context = context;
+            }
+            public async Task<IEnumerable<Employee>> Handle(GetAllEmployeesQuery request, CancellationToken cancellationToken)
+            {
+
+                var employees = await _context.Employees!.ToListAsync() ?? null;
+
+                return employees!.AsReadOnly();
+                
+            }
+        }
+    }
+}
--- Employees.API/Program.cs
+++ Employees.API/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 
+using MediatR;
+
 using Entities.Context;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +16,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
                     b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 
 var app = builder.Build();
 
