d4a8d118d8920bf63fdecba1eb3ec4c090ffc513
TobbyJay <umohjunior96@gmail.com>
2022-01-05 14:02:25 +0100

    impleented get employee by id query

--- Application/Features/EmployeeFeatures/Queries/GetEmployeeByIdQuery.cs
+++ Application/Features/EmployeeFeatures/Queries/GetEmployeeByIdQuery.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+
+using Entities;
+using Entities.Context;
 
 namespace Application.Features.EmployeeFeatures.Queries
 {
-    internal class GetEmployeeByIdQuery
+    public class GetEmployeeByIdQuery : IRequest<Employee>
     {
+        public Guid EmployeeId { get; set; }
+
+        public class GetEmployeeByIdQueryHandler : IRequestHandler<GetEmployeeByIdQuery, Employee>
+        {
+            private readonly AppDbContext _context;
+            public GetEmployeeByIdQueryHandler(AppDbContext context)
+            {
+                _context = context;
+            }
+            public async Task<Employee> Handle(GetEmployeeByIdQuery query, CancellationToken cancellationToken)
+            {
+                var employee = _context.Employees!.Where(e => e.EmployeeId == query.EmployeeId).FirstOrDefault() ?? null;
+
+                return employee!;
+            }
+        }
     }
 }
