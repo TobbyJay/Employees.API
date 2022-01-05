bce155e5a4b46abe1602dcdcfd112b7be9918ff8
TobbyJay <umohjunior96@gmail.com>
2022-01-05 14:21:09 +0100

    added create employee command

---
+++ Application/Features/EmployeeFeatures/Commands/CreateEmployee/CreateEmployeeCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Application.Features.EmployeeFeatures.Commands.CreateEmployee
+{
+    public class CreateEmployeeCommand : IRequest<Guid>
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Position { get; set; } = null!;
+        public string Department { get; set; } = null!;
+        public string Duty { get; set; } = null!;
+    }
+}
---
+++ Application/Features/EmployeeFeatures/Commands/CreateEmployee/CreateEmployeeCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using Entities;
+using Entities.Context;
+using MediatR;
+
+namespace Application.Features.EmployeeFeatures.Commands.CreateEmployee
+{
+    public class CreateEmployeeCommandHandler : IRequestHandler<CreateEmployeeCommand, Guid>
+    {
+        private readonly AppDbContext _context;
+        public CreateEmployeeCommandHandler(AppDbContext context)
+        {
+            _context = context;
+        }
+        public Task<Guid> Handle(CreateEmployeeCommand command, CancellationToken cancellationToken)
+        {
+            var employee = CreateNewEmployee(command, _context);
+
+            return employee;
+        }
+
+        private async Task<Guid> CreateNewEmployee(CreateEmployeeCommand command, AppDbContext _context)
+        {
+            var createEmployee = new Employee();
+
+            createEmployee.FirstName = command.FirstName;
+            createEmployee.LastName = command.LastName;
+            createEmployee.PhoneNumber = command.PhoneNumber;
+            createEmployee.Position = command.Position;
+            createEmployee.Duty = command.Duty;
+            createEmployee.Department = command.Department;
+            createEmployee.Address = command.Address;
+            createEmployee.Email = command.Email;
+
+            _context.Employees!.Add(createEmployee);
+            await _context.SaveChangesAsync();
+
+            return createEmployee.EmployeeId;
+        }
+    }
+}
